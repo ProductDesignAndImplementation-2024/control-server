c9430f930f1fdd7c2ea75060bb0ab4ade946c79d
Mikko Jokitalo <t2jomi00@students.oamk.fi>
2024-11-12 14:37:16 +0200

    Add initial Flask application with SSH command execution and web interface

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify, render_template
+import paramiko
+
+app = Flask(__name__)
+
+host_ip = '10.0.0.2'
+port = 5000
+
+route = ['r', 'f', 'l', 'f', 'r', 'f']
+
+current_command = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/request-route', methods=['GET'])
+def request_image():
+    try:
+        return jsonify({"route": route}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/get-command', methods=['GET'])
+def get_command():
+    global current_command
+    if current_command:
+        return jsonify({'command': current_command}), 200
+    else:
+        return jsonify({'error': 'No command available'}), 404
+
+
+@app.route('/send-command', methods=['POST'])
+def send_command():
+    global current_command
+    data = request.get_json()
+    if 'command' in data:
+        current_command = data['command']
+        return jsonify({'status': 'success', 'command': current_command}), 200
+    else:
+        return jsonify({'error': 'No command specified'}), 400
+
+
+@app.route('/run-ssh-command', methods=['POST'])
+def run_ssh_command():
+    data = request.get_json()
+    if 'command' in data:
+        command = data['command']
+        try:
+            hostname = '10.0.0.34' #GoPiGo3ONE IP
+            port = 22
+            username = 'pi'
+            password = 'robots1234'
+
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname, port, username, password)
+
+            stdin, stdout, stderr = ssh.exec_command(command)
+            output = stdout.read().decode()
+            error = stderr.read().decode()
+            print("Output: ", output)
+            ssh.close()
+            
+            if error:
+                return jsonify({'error': error}), 500
+            else:
+                return jsonify({'message': output if output else 'Command executed successfully'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    else:
+        return jsonify({'error': 'No command provided'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host=host_ip, port=port)
